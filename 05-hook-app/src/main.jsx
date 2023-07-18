a1db6e15a1118806714ec8defb3d0717406b3eaa
griceldavaldez <gvaldez@sodep.com.py>
2023-07-18 10:42:47 -0400

    Profundizando hooks - useContext
    - Configurar rutas en React
    - Link y NavLink
    - CreateContex y ContexProvider

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
+import { BrowserRouter } from 'react-router-dom'
 //import { HooksApp } from './HooksApp'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -14,9 +15,13 @@ import { TodoApp } from './08-useReducer/TodoApp'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { CallbackHook } from './06-memos/CallbackHook'
 //import { Padre } from './07-tarea-memo/Padre'
-
+//import { TodoApp } from './08-useReducer/TodoApp'
 //import './08-useReducer/intro-reducer'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <TodoApp />
+    <BrowserRouter>
+        <MainApp />
+    </BrowserRouter>
 )
