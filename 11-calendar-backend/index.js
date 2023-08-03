517660a13aa52b6d2f3f03c14052a16382695657
griceldavaldez <gvaldez@sodep.com.py>
2023-08-03 11:49:31 -0400

    Implementa backend de autenticacion con jwt

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 require('dotenv').config();
-//const cors = require('cors');
-//const { dbConnection } = require('./database/config');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
 const app = express();
 
 // Base de datos
-//dbConnection();
+dbConnection();
 
 // CORS
-//app.use(cors())
+app.use(cors())
 
 // Directorio Público
 app.use( express.static('public') );
@@ -19,7 +19,7 @@ app.use( express.static('public') );
 app.use( express.json() );
 
 // Rutas
-//app.use('/api/auth', require('./routes/auth') );
+app.use('/api/auth', require('./routes/auth') );
 // TODO: CRUD: Eventos
 
 
@@ -28,8 +28,3 @@ app.use( express.json() );
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
-
-
-
-
-
